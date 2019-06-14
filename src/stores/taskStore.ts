4ba9810b85d14604f53f371f34447f3787443562
mazhuga <mazhuga4@gmail.com>
2019-06-14 17:40:56 +0300

    add actions, config, data fetch

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,12 +1,18 @@
 import { createContext } from 'react';
-import { decorate, observable } from 'mobx';
+import { decorate, observable, configure, action } from 'mobx';
+
+configure({ enforceActions: 'observed' });
 
 export class Tasks {
+  // values
   tasks: string[] = ['Task 1', 'Task 2'];
+  // actions
+  addTask = (task: string) => this.tasks.push(task);
 }
 
 decorate(Tasks, {
   tasks: observable,
+  addTask: action,
 });
 
 export default createContext(new Tasks());
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,18 +1,48 @@
 import { createContext } from 'react';
-import { decorate, observable, computed } from 'mobx';
+import { decorate, observable, computed, configure, action } from 'mobx';
+
+configure({ enforceActions: 'observed' });
+
+interface user {
+  firstName?: string;
+  lastName?: string;
+}
 
 export class User {
+  // values
   firstName: string = '';
   lastName: string = '';
+
+  // computed
   get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
+
+  // actions
+  fetchUser = (): void => {
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(({ results }) => {
+        const { first, last } = results[0].name;
+        this.updateUser({ firstName: first, lastName: last });
+      });
+  };
+
+  updateUser = ({
+    firstName = this.firstName,
+    lastName = this.lastName,
+  }: user) => {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
 }
 
 decorate(User, {
   firstName: observable,
   lastName: observable,
   fullName: computed,
+  fetchUser: action,
+  updateUser: action,
 });
 
 export default createContext(new User());
